b31d85f91766bb5a1290083b54afc03cffc31885
rebecacalvoquintero <rebeca.quintero@orkestro.com>
2020-04-13 13:54:51 +0700

    sets up swagger and directions endpoint

---
+++ server/index.js
@@ -0,0 +1,61 @@
+require("dotenv").config();
+const Hapi = require("@hapi/hapi");
+const Inert = require("@hapi/inert");
+const Vision = require("@hapi/vision");
+const HapiSwagger = require("hapi-swagger");
+
+(async () => {
+  const server = await new Hapi.Server({
+    host: "localhost",
+    port: 8000,
+  });
+
+  const swaggerOptions = {
+    info: {
+      title: "Test API Documentation",
+      version: "v1",
+    },
+  };
+
+  server.route({
+    method: "GET",
+    path: "/directions",
+    handler: async (request, h) => {
+      const { profile, coordinates } = request.params;
+      const token = process.env.MAPBOX_TOKEN;
+      const url = `https://api.mapbox.com/directions/v5/mapbox${profile}/${coordinates}?access_token=${token}`;
+      // return
+      // const url = `https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=${process.env.API_KEY}`;
+      // try {
+      //   // const response = await rp(url);
+      //   const response = directionsResultObject;
+      //   console.log(response);
+
+      //   return response;
+      // } catch (error) {
+      //   console.log("error ", error);
+      // }
+    },
+    options: {
+      description: "Directions api",
+      notes: "Console log it",
+      tags: ["api"], // ADD THIS TAG
+    },
+  });
+
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: swaggerOptions,
+    },
+  ]);
+
+  try {
+    await server.start();
+    console.log("Server running at:", server.info.uri);
+  } catch (err) {
+    console.log(err);
+  }
+})();
